615e640a1951e8a8f7cabe59a923402b8c852314
Seth Martin Ely <sethmartinely@gmail.com>
2020-02-02 15:48:11 -0600

    updated portfolio

--- client/src/portfolio.js
+++ client/src/portfolio.js
@@ -5,6 +5,7 @@ import express_project from './img/express_project.jpg';
 import file_upload from './img/file_upload.jpg';
 import bakers_list from './img/bakers_list.jpg';
 import simple_task from './img/simple_task.jpg';
+import Random_Chord_Generator from './img/Random_Chord_Generator.png';
 
 class Portfolio extends Component {
   render() {
@@ -15,19 +16,20 @@ class Portfolio extends Component {
           and backend support. Meant to simulate the "post-it note on 
           a white-board" way of tracking progress for a project.'/>
           <Project title="Bakers List" href="https://github.com/seely1991/bakers-list-react" src={bakers_list} preview="https://bakers-list.herokuapp.com" description='My first attempt to create a full-stack web app. Simply add your ingredients to a list and save it to a database.'/>
-          <Project title="Bible API" href="https://glitch.com/edit/#!/functional-pancreas?path=server.js:1:0" src={bible_api} preview="https://functional-pancreas.glitch.me/" description='This was an attempt to make a searchable API for finding bible verses. I converted a text document into JSON format using regex expressions.'/>          
+          <Project title="Bible API" href="https://glitch.com/edit/#!/functional-pancreas?path=server.js:1:0" src={bible_api} preview="https://functional-pancreas.glitch.me/" description='This was an attempt to make a searchable API for finding bible verses. I converted a text document into JSON format using regular expressions.'/>          
+          <Project title="Random Chord Generator" href="https://github.com/seely1991/random-chord-generator" src={Random_Chord_Generator} description="A random chord/note generator useful for practicing scales, chords, or improvisational sightreading." />
           <Project title="Simple Stopwatch" src="https://s3-us-west-2.amazonaws.com/i.cdpn.io/2198892.jpwWOM.small.e3662075-3c75-4a47-baf4-f585916fe69f.png" description="A stopwatch app made with vanilla JS."/>
           <Project title="Drum Machine" src="https://s3-us-west-2.amazonaws.com/i.cdpn.io/2198892.gjBPqY.small.4e8952e3-e491-44dc-87c5-632f89b2573e.png" description="A working drum machine using vanilla JS. Use it to make some awesome beats!"/>
           <Project title="Javascript Calculator" src="https://s3-us-west-2.amazonaws.com/i.cdpn.io/2198892.EpONBM.small.e2eef502-e14f-42fa-a0b5-2d3133219795.png" description="A simple calculator using vanilla JS."/>        
           <Project title="Express Project" href="https://glitch.com/edit/#!/bouncy-market?path=server.js:1:0" src={express_project} preview="https://bouncy-market.glitch.me/" description="A project from FreeCodeCamp to perform simple API tasks."/>
-          <Project title="Exercise Database" href="https://glitch.com/edit/#!/brindle-book?path=server.js:1:0" src={exercise_database} preview="https://brindle-book.glitch.me/" description="Another FCC project practicing get and post requests in node.js."/>
+          {/*<Project title="Exercise Database" href="https://glitch.com/edit/#!/brindle-book?path=server.js:1:0" src={exercise_database} preview="https://brindle-book.glitch.me/" description="Another FCC project practicing get and post requests in node.js."/>*/}
           <Project title="D3 Chloropleth Map" src="https://s3-us-west-2.amazonaws.com/i.cdpn.io/2198892.ZMbGme.small.4931eb21-ea7b-4dc6-8349-1cecd5e9c76f.png" description="A visual representation of the percent of people who are college educated by state."/>
           <Project title="D3 Tree Map" src="https://s3-us-west-2.amazonaws.com/i.cdpn.io/2198892.RYWoGJ.small.ae6a249a-de88-4819-9b1c-ab6b8a85afbf.png" description="A tree map displaying movie tickets sold."/>
           <Project title="D3 Heat Map" src="https://s3-us-west-2.amazonaws.com/i.cdpn.io/2198892.WgvOwp.small.1c62d38e-3efe-4ebf-81a7-161f3d95b0f3.png" description="A chart showing the average temperatures for each month since 1760."/>
-          <Project title="D3 Scatter Plot" src="https://s3-us-west-2.amazonaws.com/i.cdpn.io/2198892.GXKZxj.small.df5090e8-3b23-4838-8ec1-841e9250b4d0.png" description="A scatter plot showing times for the Tour De France, and whether or not racers had been doping."/>
-          <Project title="D3 Bar Chart" src="https://s3-us-west-2.amazonaws.com/i.cdpn.io/2198892.ajgPgX.small.b5716128-6f8c-4e87-a24b-0bf284693bad.png" description="A simple bar chart showing the net gross for different quarters of a fictional business since 1947."/>
-          <Project title="Product Landing Page" src="https://s3-us-west-2.amazonaws.com/i.cdpn.io/2198892.RBgdmq.small.bddae7f3-ccea-472e-8920-646408aa4d9b.png" description="A simple product landing page for a fictional robot building company."/>
-          <Project title="Simple Form" src="https://s3-us-west-2.amazonaws.com/i.cdpn.io/2198892.KBqdEM.small.a0334fb6-b6a8-4df8-838e-9c0d014a6a99.png" description="A simple form using vanilla js and html."/>
+          {/*<Project title="D3 Scatter Plot" src="https://s3-us-west-2.amazonaws.com/i.cdpn.io/2198892.GXKZxj.small.df5090e8-3b23-4838-8ec1-841e9250b4d0.png" description="A scatter plot showing times for the Tour De France, and whether or not racers had been doping."/>
+          {/*<Project title="D3 Bar Chart" src="https://s3-us-west-2.amazonaws.com/i.cdpn.io/2198892.ajgPgX.small.b5716128-6f8c-4e87-a24b-0bf284693bad.png" description="A simple bar chart showing the net gross for different quarters of a fictional business since 1947."/>*/}
+          {/*<Project title="Product Landing Page" src="https://s3-us-west-2.amazonaws.com/i.cdpn.io/2198892.RBgdmq.small.bddae7f3-ccea-472e-8920-646408aa4d9b.png" description="A simple product landing page for a fictional robot building company."/>*/}
+          {/*<Project title="Simple Form" src="https://s3-us-west-2.amazonaws.com/i.cdpn.io/2198892.KBqdEM.small.a0334fb6-b6a8-4df8-838e-9c0d014a6a99.png" description="A simple form using vanilla js and html."/>*/}
         </div>
       </div>
     );
